2095363c68dca08e6fb11df938279d6654da9623
iahmedelbayaa <iahmedelbayaa>
2025-09-29 16:47:41 +0300

    feat: create AuthModule with JWT authentication setup and user management

---
+++ src/modules/auth.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { User } from '../entities/user.entity';
+import { AuthService } from '../services/auth.service';
+import { AuthController } from '../controllers/auth.controller';
+import { JwtStrategy } from '../strategies/jwt.strategy';
+
+@Module({
+  imports: [
+    PassportModule,
+    TypeOrmModule.forFeature([User]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get<string>('JWT_EXPIRES_IN') || '24h',
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService],
+})
+export class AuthModule {}
